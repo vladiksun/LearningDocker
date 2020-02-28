44cb4e4064e8820a6f079af795dd646ac0e262f0
vladiksun85@gmail.com <vladiksun85@gmail.com>
2020-02-28 14:51:41 +0300

    make template based configuration

--- Kubernetes/kind/OpenAM/installOpenAM.sh
+++ Kubernetes/kind/OpenAM/installOpenAM.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-kubectl apply -f openAM-pv-hostpath.yaml
-kubectl apply -f openAM-pvc.yaml
-kubectl apply -f openAM-service.yaml
-kubectl apply -f ingress-nginx-openam_v2.yaml
+kubectl apply -f openam.yaml
+
